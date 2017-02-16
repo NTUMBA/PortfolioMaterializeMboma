25a309d9a1416ddf801d80b3544b9111a01db424
NTUMBA <cityzenmboma@yahoo.fr>
2017-02-16 15:33:38 +0100

    Restaure folder

---
+++ README.md
@@ -0,0 +1,1 @@
+Restaure folder portfolio materialize
---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+// $(".theme").hide().show("slow");
+// $(document).ready(function(){
+//   $(".button-collapse").sideNav();
+// });
+$(document).ready(function(){
+  $(".button-collapse").sideNav();
+});
+
+
+$(document).ready(function(){
+          $('.carousel').carousel();
+          // $('.carousel.carousel-slider').carousel({full_width: true});
+          $('.carousel').carousel({
+    // padding: 200
+});
+autoplay()
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4500);
+}
+
+
+});
